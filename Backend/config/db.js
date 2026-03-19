dda739aeabd6a40c375c9318985a38ccddbae368
ambesh jaiswal <ambeshjaiswal92@gmail.com>
2026-03-19 15:55:10 +0530

    change db

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -6,7 +6,8 @@ const connectDB = async () => {
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
-        process.exit(1);
+        // Do not exit process, just log error so server can still start
+        // process.exit(1);
     }
 };
 
