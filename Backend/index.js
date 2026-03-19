03610498cad5e03b5337306d8e2c5699a1e2066a
ambesh jaiswal <ambeshjaiswal92@gmail.com>
2026-03-19 14:51:05 +0530

    change index

--- Backend/index.js
+++ Backend/index.js
@@ -30,15 +30,7 @@ const allowedOrigins = [
     'http://localhost:5173'
 ];
 app.use(cors({
-    origin: (origin, callback) => {
-        // allow requests with no origin (like mobile apps, curl, etc.)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        } else {
-            return callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: allowedOrigins,
     credentials: true,
 }));
 
